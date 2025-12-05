8f7b144d62a653edf2c34b9c0a264c90793745fe
AnubhavRishav <rishavanubhav96@gmail.com>
2025-12-05 20:07:39 +0530

    commit

---
+++ start-backend.ps1
@@ -0,0 +1,4 @@
+$env:JAVA_HOME = "C:\Program Files\Java\jdk-21"
+cd C:\Users\risha\Fullstack-Expense-Tracker\backend
+.\mvnw.cmd spring-boot:run
+
---
+++ start-frontend.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\risha\Fullstack-Expense-Tracker\frontend
+npm start
+
